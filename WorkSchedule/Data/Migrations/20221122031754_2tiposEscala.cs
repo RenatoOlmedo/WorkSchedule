9e9e0d31f96517a0cd2d48cc6b7e4c5ce99ff1cb
Renato <renato78563@gmail.com>
2022-11-22 00:18:32 -0300

    tipos escala

---
+++ WorkSchedule/Data/Migrations/20221122031754_2tiposEscala.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkSchedule.Data.Migrations
+{
+    public partial class _2tiposEscala : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "horasTrabalho",
+                table: "cargo",
+                newName: "Trabalho");
+
+            migrationBuilder.RenameColumn(
+                name: "horasDescanso",
+                table: "cargo",
+                newName: "Descanso");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Trabalho",
+                table: "cargo",
+                newName: "horasTrabalho");
+
+            migrationBuilder.RenameColumn(
+                name: "Descanso",
+                table: "cargo",
+                newName: "horasDescanso");
+        }
+    }
+}
--- WorkSchedule/Models/Cargo.cs
+++ WorkSchedule/Models/Cargo.cs
@@ -9,8 +9,8 @@ namespace WorkSchedule.Models
         public int id { get; set; }
         public string? nomeCargo { get; set; }
         public string? tipoEscala { get; set; }
-        public int? horasTrabalho { get; set; }
-        public int? horasDescanso { get; set; }
+        public int? Trabalho { get; set; }
+        public int? Descanso { get; set; }
         public int status { get; set; } = 1;
         public Empresa empresa { get; set; }
     }
