67212638c4d996a1d70d52bc6ee46eb070616f72
Renato <renato78563@gmail.com>
2022-11-28 01:05:56 -0300

    create escala javascript

--- WorkSchedule/Pages/Escala/createEscala.cshtml.cs
+++ WorkSchedule/Pages/Escala/createEscala.cshtml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using WorkSchedule.Data;
 using WorkSchedule.Models;
 
@@ -13,14 +15,32 @@ namespace WorkSchedule.Pages.Escala
     public class createEscalaModel : PageModel
     {
         private readonly WorkSchedule.Data.ApplicationDbContext _context;
-
-        public createEscalaModel(WorkSchedule.Data.ApplicationDbContext context)
+        private readonly UserManager<User> _userManager;
+        public Empresa empresa { get; set; }
+        public List<User> funcionarios { get; set; }
+        public createEscalaModel(WorkSchedule.Data.ApplicationDbContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGet()
         {
+            funcionarios = new List<User>();
+            var user = await _userManager.GetUserAsync(User);
+            user = await _context.user.Where(x => x.Id == user.Id).Include(x => x.empresa).FirstOrDefaultAsync();
+            empresa = user.empresa;
+
+            var pessoasEmpresa  = await _context.user.Where(x => x.empresa == empresa).Include(x => x.cargo).ToListAsync();
+            foreach (var pessoa in pessoasEmpresa)
+            {
+                var roles = await _userManager.GetRolesAsync(pessoa);
+                if (!roles.Contains("Admin"))
+                {
+                    funcionarios.Add(pessoa);
+                }
+            }
+
             return Page();
         }
 
@@ -29,17 +49,11 @@ namespace WorkSchedule.Pages.Escala
         
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int? formQntFuncionarios, string? formFunc1, string? formFunc2, string? formFunc3, string? formFunc4, string? formFunc5, int? formEmpresa)
         {
-          if (!ModelState.IsValid)
-            {
-                return Page();
-            }
 
-            _context.Escala.Add(Escala);
-            await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("./listEscala");
         }
     }
 }
---
+++ WorkSchedule/wwwroot/js/createEscala.js
@@ -0,0 +1,77 @@
+﻿$(document).ready(function () {
+    $("#geraEscala").click(function () {
+        if ($("#mes").val() != "" && $("#mes").val() != null && $("#padrao").val() != "" && $("#padrao").val() != null && $("#funcionario").val() != "" && $("#funcionario").val() != null) {
+            if (parseInt($("#p1").children("option:selected").val()) > 0) {
+                $(".p1").html($("#p1").children("option:selected").text());
+                $("#formFunc1").val($("#p1").children("option:selected").val())
+            }
+            if (parseInt($("#p2").children("option:selected").val()) > 0) {
+                $(".p2").html($("#p2").children("option:selected").text());
+                $("#formFunc2").val($("#p2").children("option:selected").val())
+            }
+            if (parseInt($("#p3").children("option:selected").val()) > 0) {
+                $(".p3").html($("#p3").children("option:selected").text());
+                $("#formFunc3").val($("#p3").children("option:selected").val())
+            }
+            if (parseInt($("#p4").children("option:selected").val()) > 0) {
+                $(".p4").html($("#p4").children("option:selected").text());
+                $("#formFunc4").val($("#p4").children("option:selected").val())
+            }
+            if (parseInt($("#p5").children("option:selected").val()) > 0) {
+                $(".p5").html($("#p5").children("option:selected").text());
+                $("#formFunc5").val($("#p5").children("option:selected").val())
+            }
+
+
+            $("#escala").removeClass("d-none");
+            $("#btnSubmit").removeClass("d-none");
+        }
+        else {
+            $("#modalAlert").modal("show");
+        }
+    });
+
+    $("#closeModal").click(function () {
+        $("#modalAlert").modal("hide");
+    });
+});
+
+function qntFuncionarios() {
+    $("#div-p1").addClass("d-none");
+    $("#div-p2").addClass("d-none");
+    $("#div-p3").addClass("d-none");
+    $("#div-p4").addClass("d-none");
+    $("#div-p5").addClass("d-none");
+
+    var qnt = parseInt($("#funcionario").children("option:selected").val());
+    switch (qnt) {
+        case 1:
+            $("#div-p1").removeClass("d-none");
+            break;
+        case 2:
+            $("#div-p1").removeClass("d-none");
+            $("#div-p2").removeClass("d-none");
+            break;
+        case 3:
+            $("#div-p1").removeClass("d-none");
+            $("#div-p2").removeClass("d-none");
+            $("#div-p3").removeClass("d-none");
+            break;
+        case 4:
+            $("#div-p1").removeClass("d-none");
+            $("#div-p2").removeClass("d-none");
+            $("#div-p3").removeClass("d-none");
+            $("#div-p4").removeClass("d-none");
+            break;
+        case 5:
+            $("#div-p1").removeClass("d-none");
+            $("#div-p2").removeClass("d-none");
+            $("#div-p3").removeClass("d-none");
+            $("#div-p4").removeClass("d-none");
+            $("#div-p5").removeClass("d-none");
+            break;
+        default:
+            break;
+    }
+    $("#formQntFuncionarios").val(qnt);
+};
